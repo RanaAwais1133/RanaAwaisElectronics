5f196f4d202236d0698664d2179196714643ecb4
huzaifash313-sudo <huzaifash313@gmail.com>
2026-07-12 12:06:01 +0500

    All Done

--- rana-awais-frontend/src/components/layout/Navbar.tsx
+++ rana-awais-frontend/src/components/layout/Navbar.tsx
@@ -10,7 +10,7 @@ interface NavbarProps {
   onMenuToggle: () => void;
 }
 
-const Navbar = React.memo<NavbarProps>(({ onMenuToggle }) => {
+const Navbar = React.memo(({ onMenuToggle }: NavbarProps) => {
   const { t } = useTranslation();
   const user = useAuthStore((s) => s.user);
   const logout = useAuthStore((s) => s.logout);
